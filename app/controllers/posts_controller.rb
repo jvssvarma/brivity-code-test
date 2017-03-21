0c35f98b3659bec1b3f5a0a77154b74700a39543
jvssvarma <jampana.varma49@gmail.com>
2017-03-21 16:10:06 -0700

    Add create tests and methods in for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,32 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show]
+
   def index
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new post_params
+    if @post.save
+      redirect_to @post, notice: "Post added successfully"
+    else
+      render 'new', notice: "Something went wrong! Try again"
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -12,4 +12,19 @@ describe 'navigate' do
       expect(page).to have_content(/All Posts/)
     end
   end
+
+  describe 'creation' do
+    it 'has a new post form that can be reached' do
+      visit new_post_path
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'can be created from the new form page' do
+      visit new_post_path
+      fill_in 'post[title]', with: "a post test"
+      fill_in 'post[body]', with: "Something in the body"
+      click_on "Save"
+      expect(page).to have_content("Something in the body")
+    end
+  end
 end
